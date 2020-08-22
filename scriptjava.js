3fe361bc8879855cfcfc639deb39c12821699368
LeoRezio <leonardo.rezio@gmail.com>
2020-08-22 21:07:38 +0100

    first commit

--- scriptjava.js
+++ scriptjava.js
@@ -1,15 +1,30 @@
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+$('#carouselExample').on('slide.bs.carousel', function (e) {
 
-  for (var i=0;i<4;i++) {
-    next=next.next();
-    if (!next.length) {
-      next=$(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+  /*
+
+  CC 2.0 License Iatek LLC 2018
+  Attribution required
+  
+  */
+
+  var $e = $(e.relatedTarget);
+  
+  var idx = $e.index();
+  console.log("IDX :  " + idx);
+  
+  var itemsPerSlide = 8;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
   }
 });
